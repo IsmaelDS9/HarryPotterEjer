c19f7fa9d220b297b9edcea023259664aa56d882
IsmaelDS9 <isma247se@gmail.com>
2023-08-29 21:50:47 -0600

    Se modifico el proyecto

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -3,43 +3,98 @@ $(document).ready(function(){
 });
 
 function tabla(){
+
 	$.ajax({
-		type:'GET',
-		url:'https://hp-api.onrender.com/api/characters',
-		contentType:'application/json; charset=UTF-8',
-		headers:{'Access-Control-Allow-Origin':'*'},
-		CORS: true,
-		crossDomain: true,
-		dataType:'JSON',
-		async: false,
+
+	method :"get",
+	url : 'https://hp-api.onrender.com/api/characters',
+	dataType: 'json',
 		
 
 
 		success: function(response){
 			console.log("respuesta--> " +JSON.stringify(response));
  		var list='';
- 		response.forEach(perso=>{
- 			console.log("respuesta--> " +JSON.stringify(perso));
+ 		
 
+ 		for(let i = 0; i <response.length;i++){
  			list+= '<tr>'+
- 			'<td>'+perso.id+'</td>'+
- 			'<td>'+perso.name+'</td>'+
- 			'<td>'+perso.alternate_names+'</td>'+
- 			'<td>'+perso.species+'</td>'+
- 			'<td>'+perso.gender+'</td>'+
- 			'<td>'+perso.house+'</td>'+
- 			'<td>'+perso.dateOfBirth+'</td>'+
- 			'<td>'+perso.yearOfBirth+'</td>'+
- 			'<td>'+perso.wizard+'</td>'+
- 			'<td>'+perso.ancestry+'</td>'+
- 			'<td>'+perso.eyeColour+'</td>'+
- 			'<td>'+perso.hairColour+'</td>'+
-
+ 			'<td>'+response[i].id+'</td>'+
+ 			'<td>'+response[i].name+'</td>'+
+ 			'<td>'+response[i].alternate_names+'</td>'+
+ 			'<td>'+response[i].species+'</td>'+
+ 			'<td>'+response[i].gender+'</td>'+
+ 			'<td>'+response[i].house+'</td>'+
+ 			'<td>'+response[i].dateOfBirth+'</td>'+
+ 			'<td>'+response[i].yearOfBirth+'</td>'+
+ 			'<td>'+response[i].wizard+'</td>'+
+ 			'<td>'+response[i].ancestry+'</td>'+
+ 			'<td>'+response[i].eyeColour+'</td>'+
+ 			'<td>'+response[i].hairColour+'</td>'+	
 
  			'</tr>';
-
- 		});
- 		$('#tabla').html(list);
+ 		}
+ 		
+ 		$('#Tabla').html(list);
+		}, error:function(resultado){
+			console.log("Error al llenar la tabla")
 		}
 	});
 }
+
+
+
+//buscar
+$('#buscarP').click(function(){
+
+   var house =$('#buscar').val();
+  
+
+   var json = {"house":house};
+   console.log("JSON a buscar->"+JSON.stringify(json));
+
+   var json ='';
+
+if(house !== ''){
+   json ={house};
+
+}
+$.ajax({ 
+      type:'ajax',
+      method: 'get',
+      url:'https://hp-api.onrender.com/api/characters',
+      data: JSON.stringify(json),
+
+      
+
+      success : function(responce){
+         console.log("Responce -> Respuesta el servidor ->"+JSON.stringify(responce));
+         
+         var lista1 = '';
+
+         responce.forEach(objetos => {
+            console.log("Objectos "+JSON.stringify(objetos));
+            
+            lista1 += '<tr>'+
+ 			'<td>'+objetos.id+'</td>'+
+ 			'<td>'+objetos.name+'</td>'+
+ 			'<td>'+objetos.alternate_names+'</td>'+
+ 			'<td>'+objetos.species+'</td>'+
+ 			'<td>'+objetos.gender+'</td>'+
+ 			'<td>'+objetos.house+'</td>'+
+ 			'<td>'+objetos.dateOfBirth+'</td>'+
+ 			'<td>'+objetos.yearOfBirth+'</td>'+
+ 			'<td>'+objetos.wizard+'</td>'+
+ 			'<td>'+objetos.ancestry+'</td>'+
+ 			'<td>'+objetos.eyeColour+'</td>'+
+ 			'<td>'+objetos.hairColour+'</td>'+	
+
+ 			'</tr>';
+
+           })
+$('Tabla').html(lista1);
+
+}
+});
+
+});
